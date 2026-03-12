473e88829c5e5d3d445eea9ac6c0727b38e4578a
Radim <admin@radas.net>
2026-03-12 10:36:47 +0100

    x

--- components/somfy/somfy.h
+++ components/somfy/somfy.h
@@ -28,6 +28,7 @@ protected:
 public:
   void setup() override {
     this->emitter_pin_->setup();
+    this->emitter_pin_->pin_mode(gpio::FLAG_OUTPUT);
     this->emitter_pin_->digital_write(false);
     storage_ = new EsphomeRollingCodeStorage(remote_address_);
     remote_ = new SomfyRemote(emitter_pin_, remote_address_, storage_);
