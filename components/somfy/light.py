75d86151a9aae336f0b131f99e8f05b735099660
Radim <admin@radas.net>
2026-03-17 21:26:13 +0100

    rt

--- components/somfy/light.py
+++ components/somfy/light.py
@@ -1,6 +1,6 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.components import light, cc1101
+from esphome.components import light, cc1101, remote_transmitter
 from esphome import pins
 from esphome.const import (
     CONF_FREQUENCY,
@@ -19,20 +19,22 @@ CONF_SOMFY_PIN = "pin"
 CONF_SOMFY_RF_FREQ = "rf_read_freq"
 CONF_SOMFY_REPEAT = "repeat"
 CONF_SOMFY_CC1101 = "cc1101"
+ONF_REMOTE_TRANSMITTER = "remote_transmitter"
 
 CONFIG_SCHEMA = cv.All(
   light.light_schema(SomfyLightOutput, light.LightType.BINARY).extend(
     {
         cv.Required(CONF_SOMFY_PIN): pins.internal_gpio_output_pin_schema,
-        cv.Required(CONF_SOMFY_REMOTE_ADDRESS): cv.int_range(min=0, max=0xFFFFFFFF),        
+        cv.Required(CONF_SOMFY_REMOTE_ADDRESS): cv.int_range(min=0, max=0xFFFFFFFF),
         cv.Optional(CONF_SOMFY_REPEAT, default=4): cv.int_range(min=1, max=16),
         cv.Optional(CONF_SOMFY_RF_FREQ, default="433.92MHz"): cv.All(
           cv.frequency, cv.float_range(min=300.0e6, max=928.0e6)
         ),
         cv.Optional(CONF_FREQUENCY, default="433.42MHz"): cv.All(
           cv.frequency, cv.float_range(min=300.0e6, max=928.0e6)
-        ),        
+        ),
         cv.Required(CONF_SOMFY_CC1101): cv.use_id(cc1101.CC1101Component),
+        cv.Required(CONF_REMOTE_TRANSMITTER): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
     }
   ).extend(cv.COMPONENT_SCHEMA),
  cv.only_on([PLATFORM_ESP32, PLATFORM_ESP8266])
@@ -47,6 +49,9 @@ async def to_code(config):
 
     cc1101 = await cg.get_variable(config[CONF_SOMFY_CC1101])
     cg.add(var.set_cc1101(cc1101))
+    remote_transmitter = await cg.get_variable(config[CONF_REMOTE_TRANSMITTER])
+    cg.add(var.set_remote_transmitter(remote_transmitter))
+
 
     cg.add(var.set_remote_address(config[CONF_SOMFY_REMOTE_ADDRESS]))
     cg.add(var.set_rf_freq(config[CONF_SOMFY_RF_FREQ]))
--- components/somfy/somfy.h
+++ components/somfy/somfy.h
@@ -3,6 +3,7 @@
 #include "esphome/components/cover/cover.h"
 #include "esphome/components/cc1101/cc1101.h"
 #include "esphome/core/component.h"
+#include "esphome/components/remote_transmitter/remote_transmitter.h"
 #include "SomfyRemote.h"
 #include "EsphomeRollingCodeStorage.h"
 
@@ -24,6 +25,92 @@ protected:
   int repeat_;
   cc1101::CC1101Component *cc1101_;
   float rf_freq_, somfy_freq_;
+  remote_transmitter::RemoteTransmitterComponent *remote_transmitter_{nullptr};
+
+
+  void send_command(Command command) {
+    const uint16_t rollingCode = this->storage_->nextCode();
+    uint8_t frame[7];
+    build_frame(frame, command, rollingCode);
+    remote_base::RawTimings t;
+    build_timings(t, frame, 2);
+    for (int i = 0; i < this->repeat_count_; i++) {
+      build_timings(t, frame, 7);
+    }
+    auto call = this->remote_transmitter_->transmit();
+    call.get_data()->set_data(t);
+    call.perform();
+  }
+
+  void build_frame(uint8_t *frame, Command command, uint16_t code) {
+    const uint8_t button = static_cast<uint8_t>(command);
+    frame[0] = 0xA7;          // Encryption key. Doesn't matter much
+    frame[1] = button << 4;   // Which button did  you press? The 4 LSB will be the checksum
+    frame[2] = code >> 8;     // Rolling code (big endian)
+    frame[3] = code;          // Rolling code
+
+    frame[4] = this->remote_code_ >> 16;  // Remote address
+    frame[5] = this->remote_code_ >> 8;   // Remote address
+    frame[6] = this->remote_code_;        // Remote address
+
+    // Checksum calculation: a XOR of all the nibbles
+    uint8_t checksum = 0;
+    for (uint8_t i = 0; i < 7; i++) {
+      checksum = checksum ^ frame[i] ^ (frame[i] >> 4);
+    }
+    checksum &= 0b1111;  // We keep the last 4 bits only
+
+    // Checksum integration
+    frame[1] |= checksum;
+
+    // Obfuscation: a XOR of all the bytes
+    for (uint8_t i = 1; i < 7; i++) {
+      frame[i] ^= frame[i - 1];
+    }
+  }
+
+  void build_timings(remote_base::RawTimings & t, uint8_t *frame, uint8_t sync) {
+    const int32_t SYMBOL = 640;
+
+    if (sync == 2) {  // Only with the first frame.
+      // Wake-up pulse & Silence
+      send_high(t, 9415);
+      send_low(t, 9565 + 80000); // was delay(80)
+    }
+
+    // Hardware sync: two sync for the first frame, seven for the following ones.
+    for (int i = 0; i < sync; i++) {
+      send_high(t, 4 * SYMBOL);
+      send_low(t, 4 * SYMBOL);
+    }
+
+    // Software sync
+    send_high(t, 4550);
+    send_low(t, SYMBOL);
+
+    // Data: bits are sent one by one, starting with the MSB.
+    for (uint8_t i = 0; i < 56; i++) {
+      if (((frame[i / 8] >> (7 - (i % 8))) & 1) == 1) {
+        send_low(t, SYMBOL);
+        send_high(t, SYMBOL);
+      } else {
+        send_high(t, SYMBOL);
+        send_low(t, SYMBOL);
+      }
+    }
+
+    // Inter-frame silence
+    send_low(t, 415 + 30000); // was delay(30)
+  }
+
+  void send_high(remote_base::RawTimings & t, int32_t durationUsecs) {
+    t.push_back(static_cast<int32_t>(durationUsecs));
+  }
+
+  void send_low(remote_base::RawTimings & t, int32_t durationUsecs) {
+    t.push_back(-static_cast<int32_t>(durationUsecs));
+  }
+
 
 public:
   void setup() override {
@@ -54,7 +141,8 @@ public:
     delay(10);
     cc1101_->begin_tx();
     ESP_LOGD(TAG, "Sending %dx command: 0x%x", this->repeat_, command);
-    remote_->sendCommand(command, this->repeat_);
+    //remote_->sendCommand(command, this->repeat_);
+    send_command(command);
     delay(10);
     ESP_LOGD(TAG, "Entering RX with freq:: %.0fHz", this->rf_freq_);
     cc1101_->set_idle();
@@ -74,6 +162,7 @@ public:
   void set_cc1101(cc1101::CC1101Component *cc1101) { this->cc1101_ = cc1101; }
   void set_rf_freq(float freq) { this->rf_freq_ = freq; }
   void set_somfy_freq(float freq) { this->somfy_freq_ = freq; }
+  void set_remote_transmitter(remote_transmitter::RemoteTransmitterComponent *t) { this->remote_transmitter_ = t; }
 };
 
 } // namespace somfy
