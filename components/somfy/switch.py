b4d3532a97f8ab58e433aefb8db01f476a87b882
Radim <admin@radas.net>
2026-03-19 12:19:02 +0100

    x

--- components/somfy/switch.py
+++ components/somfy/switch.py
@@ -2,12 +2,12 @@ from esphome import automation
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import switch, cc1101, remote_transmitter, button
-from esphome.const import (    
+from esphome.const import (
     PLATFORM_ESP32,
     PLATFORM_ESP8266,
 )
 
-DEPENDENCIES = ["cc1101", "remote_transmitter"]
+DEPENDENCIES = ["remote_transmitter"]
 AUTO_LOAD = ["switch", "button"]
 
 somfy_ns = cg.esphome_ns.namespace("somfy")
@@ -15,7 +15,6 @@ SomfySwitch = somfy_ns.class_("SomfySwitch", switch.Switch, cg.Component)
 
 CONF_SOMFY_REMOTE_ADDRESS = "remote_address"
 CONF_SOMFY_REPEAT = "repeat"
-CONF_SOMFY_CC1101 = "cc1101"
 CONF_REMOTE_TRANSMITTER = "remote_transmitter"
 CONF_PROG_BUTTON = "prog_button"
 CONF_ON_TRANSMIT = "on_transmit"
@@ -28,7 +27,6 @@ CONFIG_SCHEMA = cv.All(
         cv.Optional(CONF_SOMFY_REPEAT, default=4): cv.int_range(min=1, max=16),
         cv.Optional(CONF_ON_TRANSMIT): automation.validate_automation(single=True),
         cv.Optional(CONF_ON_COMPLETE): automation.validate_automation(single=True),
-        cv.Required(CONF_SOMFY_CC1101): cv.use_id(cc1101.CC1101Component),
         cv.Required(CONF_REMOTE_TRANSMITTER): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
         cv.Optional(CONF_PROG_BUTTON): cv.use_id(button.Button),
     }
@@ -41,11 +39,9 @@ async def to_code(config):
     var = await switch.new_switch(config)
     await cg.register_component(var, config)
 
-    cc1101 = await cg.get_variable(config[CONF_SOMFY_CC1101])
-    cg.add(var.set_cc1101(cc1101))
     remote_transmitter = await cg.get_variable(config[CONF_REMOTE_TRANSMITTER])
     cg.add(var.set_remote_transmitter(remote_transmitter))
-    
+
     if CONF_PROG_BUTTON in config:
       btn = await cg.get_variable(config[CONF_PROG_BUTTON])
       cg.add(var.set_prog_button(btn))
@@ -60,5 +56,5 @@ async def to_code(config):
             var.get_complete_trigger(), [], on_complete_config
         )
 
-    cg.add(var.set_remote_address(config[CONF_SOMFY_REMOTE_ADDRESS]))    
+    cg.add(var.set_remote_address(config[CONF_SOMFY_REMOTE_ADDRESS]))
     cg.add(var.set_repeat(config[CONF_SOMFY_REPEAT]))
